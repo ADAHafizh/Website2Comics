ae43ecfe82b831a927897c5a0b90a34bbc3d5848
Ahmad Dahlan Hafizh <144985531+ADAHafizh@users.noreply.github.com>
2023-10-08 15:11:25 +0400

    1.0 - Initialized website
    - Installed all components
    - Included assets
    - Included libraries
    - Included plugins

---
+++ js/script.js
@@ -0,0 +1,24 @@
+anime({
+    targets: ".bg0, .bg2, .bg3, .bg4, .bg5",
+    translateY: [50, 0],
+    direction: 'alternate',
+    easing: 'easeInOutSine',
+    delay: function (el, i, l) {
+        return i * 100;
+    },
+    loop: false,
+});
+
+anime.timeline({loop: false})
+    .add({
+        targets: '.ml15 .word',
+        scale: [14,1],
+        opacity: [0,1],
+        easing: "easeOutCirc",
+        duration: 1000,
+        delay: (el, i) => 800 * i
+    })
+    .add({
+        target: 'start_button',
+        opacity: [0,1],
+})
